4a6a7b9897564f9b2561388bc4fd00adb805430a
imartinezgmxus <imartinez@gmx.us>
2017-11-20 23:45:24 -0600

    Add a convert variable example

--- main.go
+++ main.go
@@ -8,4 +8,16 @@ func main() {
 	var x string
 	x = "¡Hola Mundo!"
 	fmt.Println(x)
+	ConvertPlus()
+}
+
+// ConvertPlus: Convert a string variable to Float64
+func ConvertPlus() {
+	a := 7
+	b := 14.0
+
+	fmt.Printf("Tipo de variable para A %T \n", a)
+	fmt.Printf("Tipo de variable para B %T \n", b)
+
+	fmt.Println("Resultado de la suma: ", float64(a)+b)
 }
