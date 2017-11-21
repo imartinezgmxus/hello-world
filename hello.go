86183ea74ada155058c42d7cd63f3c413c66a72a
imartinezgmxus <imartinez@gmx.us>
2017-11-20 20:09:52 -0600

    Change format

--- hello.go
+++ hello.go
@@ -6,5 +6,5 @@ func main() {
 	fmt.Printf("Hello world...\n")
 	fmt.Printf("Hello go...\n")
 	fmt.Printf("First commit in github.com\n")
-	fmt.Printf("First try commit with ssh key")
+	fmt.Printf("First try commit with ssh key\n")
 }
